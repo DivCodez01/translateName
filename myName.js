5a71a46aed65357e7a4dd6f09f9aa414ddffe3df
divin-gakiza <divin.gakiza01@gmail.com>
2024-04-21 13:37:47 +0200

    changeError

--- myName.js
+++ myName.js
@@ -1,9 +1,9 @@
 document.title = "My Name";
 const insParagraph = document.querySelector(".inside-paragraph");
-const parag1 = document.getElementById("parag1");
-const parag2 = document.getElementById("parag2");
-const parag3 = document.getElementById("parag3");
-const parag4 = document.getElementById("parag4");
+const parag1 = document.querySelector(".parag1");
+const parag2 = document.querySelector(".parag2");
+const parag3 = document.querySelector(".parag3");
+const parag4 = document.querySelector(".parag4");
 const button = document.querySelector("button");
 
 button.addEventListener("click", function () {
